e46cbbe2577c44b0ef5e5808a43818fe16a2d2e4
Piotr <p.lukasik@gmail.com>
2024-03-20 13:32:36 -0400

    Update split_amplicon_libs.py
    version 1.1

--- split_amplicon_libs.py
+++ split_amplicon_libs.py
@@ -6,7 +6,7 @@ nucl_ambigs = {"R": "[AG]", "Y": "[CT]", "S": "[GC]", "W": "[AT]", "K": "[GT]", 
 
 
 if len(sys.argv) != 4:
-	sys.exit('\nsplit_amplicon_libs_VarLenIns.py v. 1.0. Piotr Lukasik, 23rd Dec 2020\n'
+	sys.exit('\nsplit_amplicon_libs_VarLenIns.py v. 1.1 Piotr Lukasik, 3rd Feb 2024\n'
 	         '-------------------------------------------------------------\n'
 	         'This script splits up paired fastq files corresponding to a multiplex amplicon library\n'
 	         'into a collection of fastq file pairs corresponding to different targeted regions,\n'
@@ -24,7 +24,7 @@ if not os.path.exists(Output_Dir):
     os.makedirs(Output_Dir)
 
 
-print('\nsplit_amplicon_libs.py v. 1.0. Piotr Lukasik, 23rd Dec 2020\n'
+print('\nsplit_amplicon_libs.py v. 1.1 Piotr Lukasik, 3rd Feb 2024\n'
 	   '-------------------------------------------------------------\n')
 
 
@@ -39,7 +39,7 @@ print("Reading the list of targets .....", end="")
 Target_List_Imported = []
 TARGET_LIST = open(Target_List, 'r', encoding="utf-8")
 for line in TARGET_LIST:
-    if not line.startswith("#") and len(line) > 0:
+    if not line.startswith("#") and len(line.strip()) > 0:
         if len(line.strip().split()) != 3:
             print("There is a problem with this line, which will be skipped:\n    ", end = "")
             print(line.strip())
@@ -56,14 +56,14 @@ for target in Target_List_Imported:
     if len(target) == 0:
         sys.exit("Problem with the imported Target List :|")
 
-    primer_R1_ambig = ".{0,4}" # assumes that there can be up to four of whatever prior to the sequence!
+    primer_R1_ambig = ".{0,6}" # assumes that there can be up to six of whatever prior to the sequence!
     for base in target[1][1:]:     # skips the first base in the primer!
         if base in nucl_ambigs.keys():
             primer_R1_ambig += nucl_ambigs[base] 
         else: 
             primer_R1_ambig += base 
 
-    primer_R2_ambig = ".{0,4}"   # assumes that there can be up to four of whatever prior to the sequence!
+    primer_R2_ambig = ".{0,6}"   # assumes that there can be up to six of whatever prior to the sequence!
     for base in target[2][1:]:       # skips the first base in the primer! Helpful for COI!
         if base in nucl_ambigs.keys():
             primer_R2_ambig += nucl_ambigs[base] 
@@ -80,10 +80,13 @@ print("")
 
 
 
-### Creating a bunch of output folders --- one for each target
+### Creating a bunch of output folders --- one for each target, and Unclassified
 for target in Target_Dict.keys():
     if not os.path.exists('%s/%s' % (Output_Dir, target)):
         os.makedirs('%s/%s' % (Output_Dir, target))
+
+if not os.path.exists('%s/Unclassified' % Output_Dir):
+    os.makedirs('%s/Unclassified' % Output_Dir)
 
 ### Creating the framework for storing read numbers for each target and sample
 Target_Counts_per_Sample = [['']]
@@ -129,19 +132,17 @@ for library in Sample_List_Imported:
     Sample_index += 1
     print("Sorting data for sample %d / %d:   %s ................." % (Sample_index, Sample_count, library[0]), end = "")
     
-    Unclassified_ct = 0
-    
     ### Now, reading in R1 and R2 fastq files. 
     READ1 = open(library[1], "r")
     READ2 = open(library[2], "r")
     
-    ### Here, we will store data for reads classified to different targets. Skipping Unclassified!
+    ### Here, we will store data for reads classified to different targets. Remembering about Unclassified!
     reads_by_target = {}
     for target in Target_Dict.keys():
         reads_by_target[target] = []
     
-    Unclassified_ct = 0   
-    
+    reads_by_target["Unclassified"] = []
+        
     
     ### Now, reading input fastqs: read pair by read pair, saving details to reads_by_target dictionary
     for line in READ1:
@@ -155,20 +156,26 @@ for library in Sample_List_Imported:
         READ2.readline()
         R2_Qual = READ2.readline().strip()
         
-        Unclassified = 1
+        unclassified = 1
         for target in Target_Dict.keys():
             #print(target, "\n", R1_Seq[:30], Target_Dict[target][0], re.match(Target_Dict[target][0], R1_Seq))
             #print(R2_Seq[:30], Target_Dict[target][1], re.match(Target_Dict[target][1], R2_Seq))
-            if bool(re.match(Target_Dict[target][0], R1_Seq)) and bool(re.match(Target_Dict[target][1], R2_Seq)): # if the primer seqs match the beginnings of target seqs
+            if bool(re.match(Target_Dict[target][0], R1_Seq)) and bool(re.match(Target_Dict[target][1], R2_Seq)): # if the primer seqs (preceded by up to six nts) match the beginnings of target seqs
                 reads_by_target[target].append([R1_Heading, R1_Seq, R1_Qual, R2_Heading, R2_Seq, R2_Qual])
                 #print("HIT!")
-                Unclassified = 0
+                unclassified = 0
+                break
         
-        Unclassified_ct += Unclassified
-
+        if unclassified == 1:
+            reads_by_target["Unclassified"].append([R1_Heading, R1_Seq, R1_Qual, R2_Heading, R2_Seq, R2_Qual])
+    
+    #for target in Target_Dict.keys():
+    #    print(target, len(reads_by_target[target]))    
+    #print("Unclassified", len(reads_by_target["Unclassified"]))
 
     ### Now, exporting contents of reads_by_target dictionary
-    for target in Target_Dict.keys():
+    Bins_to_export = list(Target_Dict.keys()) + ["Unclassified"]
+    for target in Bins_to_export:
         target_R1 = open("%s/%s/%s_%s_R1.fastq" % (Output_Dir, target, target, library[0]), "w")
         for seq in reads_by_target[target]:
             print(seq[0],seq[1],"+",seq[2], sep = "\n", file = target_R1)
@@ -181,16 +188,14 @@ for library in Sample_List_Imported:
 
 
     ### Preparing the line for Target_Counts_per_Sample, adding it
-    Read_total = Unclassified_ct
+    Read_total = 0
     
     Target_Counts_per_Sample_line = [library[0]]
-    for target in sorted(list(Target_Dict.keys())):
+    for target in Bins_to_export:
         Target_Counts_per_Sample_line.append(len(reads_by_target[target]))
         Read_total += len(reads_by_target[target])
-    Target_Counts_per_Sample_line.append(Unclassified_ct)
     
     Target_Counts_per_Sample.append(Target_Counts_per_Sample_line)
-
 
     ### Looks like it's all OK for this sample!
     print("OK! %d paired-end sequences classified!" % Read_total)
@@ -205,11 +210,22 @@ Summary_file = open(Output_Dir + "/000_splitting_summary.txt", "w")
 
 print("\nJob complete!\n")
 print("Target counts in different samples:")
-for line in Target_Counts_per_Sample:
-    for item in line:
+
+for item in Target_Counts_per_Sample[0]:
+    print(item, end = "\t")
+    print(item, end = "\t", file = Summary_file)
+print("Total")
+print("Total", file = Summary_file)
+
+for line in Target_Counts_per_Sample[1:]:
+    print(line[0], end = "\t")
+    print(line[0], end = "\t", file = Summary_file)    
+    total = 0
+    for item in line[1:]:
+        total += int(item)
         print(item, end = "\t")
         print(item, end = "\t", file = Summary_file)
-    print("")
-    print("", file = Summary_file)
+    print(total)
+    print(total, file = Summary_file)
 
 print("DONE!")            
